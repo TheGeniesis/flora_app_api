df1cc3d787083d655ba3619d4d7d9ba462067ae0
Grzegorz Matuszak <geniesis@o2.pl>
2021-10-20 18:38:37 +0200

    fully working consumerfor create measurement

--- app.py
+++ app.py
@@ -3,14 +3,27 @@ from src.services.core.config.Config import Config
 from src.services.core.dispatcher.EventDispatcher import EventDispatcher
 from src.services.core.loader.consumer import load_consumers
 from src.services.core.loader.listener import load_listeners
+import logging
 
 import os
 
+
 if __name__ == "__main__":
+
+    app = App().get_app()
     config = Config()
     config.init({
         "path": os.getcwd()
     })
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        handlers=[
+            logging.FileHandler("debug.log"),
+            logging.StreamHandler()
+        ]
+    )
 
     load_listeners()
 
@@ -18,8 +31,8 @@ if __name__ == "__main__":
 
     load_consumers()
 
-app = App().get_app()
-app.run(port=8004)
+    app.run(port=8004)
+
 
 # from flask import Flask, redirect, url_for
 # from flask_dance.contrib.authentiq import make_authentiq_blueprint
