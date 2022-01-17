423f4e51fa1883ae3f17b22decc18a405b15aa7d
Grzegorz Matuszak <geniesis@o2.pl>
2022-01-17 18:09:18 +0100

    prometheus from app works

--- src/controllers/metric.py
+++ src/controllers/metric.py
@@ -1,10 +1,12 @@
 from src.services.domain.prometheus.measure_summary import set_measure_summary
-
+from flask import Response
 from prometheus_client import CollectorRegistry, generate_latest
 
 
 def index():
-    registry = CollectorRegistry()
-    set_measure_summary(registry)
+    graphs = set_measure_summary()
+    res = []
+    for k, v in graphs.items():
+        res.append(generate_latest(v))
 
-    return generate_latest(registry)
+    return Response(res, mimetype="text/plain")
--- src/services/domain/prometheus/measure_summary.py
+++ src/services/domain/prometheus/measure_summary.py
@@ -6,15 +6,24 @@ from src.services.core.redis.Redis import Redis
 from prometheus_client import CollectorRegistry, generate_latest
 
 
-def set_measure_summary(registry: CollectorRegistry):
+def set_measure_summary():
+    graphs = {}
+
     redis = Redis().get_redis()
     labels = ['device_id', 'device_name', 'env', 'app', 'date']
+    cr = CollectorRegistry()
+
     for key in redis.scan_iter('prometheus_summar*'):
         data = json.loads(redis.get(key))
-        h = Summary(data['name'], data['description'], labels, registry=registry)
-        h.labels(data['labels']['device_id'], data['labels']['device_name'], data['labels']['env'],
-                 data['labels']['app_name'], data['labels']['date']).observe(data['value'])
+        if not graphs.get(data['name']):
+            graphs[data['name']] = Summary(data['name'], data['description'], labels, registry=cr)
+
+        graphs.get(data['name']).labels(data['labels']['device_id'], data['labels']['device_name'],
+                                        data['labels']['env'], data['labels']['app_name'],
+                                        data['labels']['date']).observe(data['value'])
 
         logger = logging.getLogger('measurement')
         logger.info('Cleaning measurement from redis: %s', key)
         redis.delete(key)
+
+    return graphs
